61f407af2568fc542b4b8981b7e6642fc4401369
smarthug <kirklayer@gmail.com>
2022-09-04 11:56:45 +0900

    wow

--- src/page/myTweak.js
+++ src/page/myTweak.js
@@ -1,9 +1,10 @@
 import * as THREE from 'three'
-import { Suspense, useRef } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Suspense, useEffect, useRef, useState } from 'react'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { useGLTF, OrbitControls, Sky, Environment, Cloud } from '@react-three/drei'
 import { Debug, Physics, RigidBody } from '@react-three/rapier'
 import { useControls, button } from 'leva'
+
 
 export default function App() {
     const { debug } = useControls({
@@ -31,14 +32,14 @@ export default function App() {
                         <Cylinder position={[-1.5, 5, 0]} rotation={[Math.PI / 2, 0, 0]} />
                         <Cylinder position={[1.75, 8, 0]} rotation={[Math.PI / 2, 0, 0]} />
                         <Track position={[-3, 0, 10.5]} rotation={[0, -0.4, 0]} />
-                        <Box position={[-3, 11, 0]} rotation={[0, 0, -0.5]} />
-                        <Box position={[-8.6, 12.3, 0]} length={80} rotation={[0, 0, -0.1]} />
+                        <Box position={[-3, 10, 0]} rotation={[0, 0, -0.5]} />
+                        <Box position={[-44.6, 12.3, 0]} length={80} rotation={[0, 0, -0.03]} />
                         <Sphere position={[-12, 13, 0]} />
                         <Sphere position={[-9, 13, 0]} />
                         <Sphere position={[-6, 13, 0]} />
                         {/* <Sphere position={[-6, 13, 0]} /> */}
-                        {Array.from({ length: 10 }, (_, i) => (
-                            <Sphere key={i} position={[-12-(i*3), 13+i, 0]} />
+                        {Array.from({ length: 20 }, (_, i) => (
+                            <Sphere key={i} position={[-12 - (i * 3), 13 + i, 0]} />
                         ))}
                         <Pacman />
                     </group>
@@ -62,23 +63,74 @@ const Box = ({ length = 4, ...props }) => (
     </RigidBody>
 )
 
-const Sphere = (props) => (
-    <RigidBody colliders="ball" restitution={0.7}>
-        <mesh castShadow receiveShadow {...props}>
-            <sphereGeometry args={[0.5, 32, 32]} />
-            <meshStandardMaterial color="white" />
-        </mesh>
-    </RigidBody>
-)
-console.log(Sphere);
-const Cylinder = (props) => (
-    <RigidBody colliders="hull" type="fixed">
-        <mesh castShadow receiveShadow {...props}>
-            <cylinderGeometry args={[0.25, 0.25, 4]} />
-            <meshStandardMaterial />
-        </mesh>
-    </RigidBody>
-)
+function Sphere(props) {
+    const ref = useRef()
+    const { viewport, camera } = useThree()
+    const { width, height } = viewport.getCurrentViewport(camera, [0, 0, 0])
+
+    const [data] = useState({
+        x: THREE.MathUtils.randFloatSpread(2),
+        y: THREE.MathUtils.randFloatSpread(height),
+        rX: Math.random() * Math.PI,
+        rY: Math.random() * Math.PI,
+        rZ: Math.random() * Math.PI,
+    })
+
+    useFrame((state) => {
+        // ref.current.rotation.set((data.rX += 0.001), (data.rY += 0.001), (data.rZ += 0.001))
+        // ref.current.position.set(data.x * width, (data.y += 0.025), 0)
+        if (ref.current.position.y > height) {
+            ref.current.position.y = -height
+        }
+    })
+
+    return (
+        <RigidBody colliders="ball" restitution={0.7}>
+            <mesh ref={ref} castShadow receiveShadow {...props}>
+                <sphereGeometry args={[0.5, 32, 32]} />
+                <meshStandardMaterial color="white" />
+            </mesh>
+        </RigidBody>
+    )
+}
+
+
+
+// const Cylinder = (props) => (
+//     <RigidBody colliders="hull" type="fixed">
+//         <mesh castShadow receiveShadow {...props}>
+//             <cylinderGeometry args={[0.25, 0.25, 4]} />
+//             <meshStandardMaterial />
+//         </mesh>
+//     </RigidBody>
+// )
+
+
+function Cylinder({position,rotation}) {
+
+
+    const ref = useRef()
+    const random = Math.floor(Math.random()*4)+1
+    useFrame((state) => {
+        const t = state.clock.getElapsedTime()
+        ref.current.setNextKinematicTranslation({ x: 3+Math.sin(t * 10)/random, y: 2+Math.sin(t * 10)/random, z: 0 })
+        // ref.current.setNextKinematicRotation({ x: Math.cos(t) * 0.2, y: Math.sin(t) * 0.1 - 0.1, z: Math.cos(t) * 0.05 })
+    })
+
+    useEffect(()=>{
+        console.log(ref.current);
+    },[])
+
+
+    return (
+        <RigidBody ref={ref} colliders="hull" type="kinematicPosition">
+            <mesh castShadow receiveShadow position={position} rotation={rotation}>
+                <cylinderGeometry args={[0.25, 0.25, 4]} />
+                <meshStandardMaterial />
+            </mesh>
+        </RigidBody>
+    )
+}
 
 function Pacman() {
     const ref = useRef()
